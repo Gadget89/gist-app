fce572d34ae509c9ba256f74a7843ca0fe9ec589
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-02-06 11:56:48 -0600

    Added static clear/save buttons in new note form. Also changed naming convention for new note form components.

--- js/Notes.js
+++ js/Notes.js
@@ -4,23 +4,26 @@ export default React.createClass({
   render() {
     return (
       <section className="gist_form">
-        <p>New Gist</p>
-        <form>
+        <p className="new_gist_title">New Gist</p>
+        <button className="new_gist_save_button">Save</button>
+        <button className="new_gist_clear_button">Clear</button>
+        <form className="new_gist_wrapper">
           <input
-            className="gist_input"
+            className="new_gist_input"
             placeholder="Title"
             type="text">
           </input>
           <input
-            className="gist_input"
+            className="new_gist_input"
             placeholder="URL"
             type="text">
           </input>
           <input
-            className="gist_input"
+            className="new_gist_input"
             placeholder="Make a note"
             type="text">
           </input>
+
         </form>
       </section>
     )
